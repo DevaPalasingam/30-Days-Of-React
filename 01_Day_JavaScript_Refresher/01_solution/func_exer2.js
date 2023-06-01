1f9d13cc92f1ba813d686970c5dce40934cc27ba
Palasingam <dpalasingam@teamscci.local>
2023-06-01 16:03:12 -0500

    finished func exercise 2

--- 01_Day_JavaScript_Refresher/01_solution/func_exer2.js
+++ 01_Day_JavaScript_Refresher/01_solution/func_exer2.js
@@ -13,11 +13,63 @@ function solveQuadratic(a, b, c) {
   let x1 = (b * -1 + Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
   let x2 = (b * -1 - Math.sqrt(b ** 2 - 4 * a * c)) / (2 * a);
 
-  if (!(x1 && x2)) {
+  if (isNaN(x1) && isNaN(x2)) {
     return "Invalid Input";
   } else if (x1 === x2) {
     return `{${x1}}`;
   } else {
     return `{${x1}, ${x2}}`;
   }
+}
+
+// Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+function printArray(arr) {
+  if (arr.length === 0) {
+    console.log("This array is empty");
+  }
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+// Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
+function showDateTime() {
+  const today = new Date(Date.now());
+  console.log(
+    today.toLocaleDateString(undefined, {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }) +
+      " " +
+      today.getHours() +
+      ":" +
+      today.getMinutes()
+  );
+}
+
+// Declare a function name reverseArray. It takes array as a parameter and it returns the reverse of the array (don't use method).
+function reverseArray(arr) {
+  if (arr.length === 0) {
+    return "This array is empty";
+  }
+  let newArr = [];
+  for (let i = arr.length; i >= 0; i--) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+// Declare a function name userIdGenerator. When this function is called it generates seven character id
+function userIdGenerator() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const length = 7;
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 }
